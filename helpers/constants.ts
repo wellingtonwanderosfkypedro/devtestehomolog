501801127e2f4866a44eab1f60cfdcf5692eec3c
wellington.pedro <wellington.pedro@agenciam3.com>
2024-03-15 16:19:38 -0300

    fix: pages

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NEXT_PUBLIC_WORDPRESS_API_ENDPOINT;
+export const API_URL = process.env.NEXT_PUBLIC_WORDPRESS_API_ENDPOINT_REST as string;
--- pages/prevencao-perdas/index.tsx
+++ pages/prevencao-perdas/index.tsx
@@ -1,13 +1,5 @@
-import CompanySegments from "@/components/CompanySegments/CompanySegments";
-import CustomerExperience from "@/components/CustomerExperience/CustomerExperience";
-import { Faq } from "@/components/Faq";
-import FormAnalyticalSolutions from "@/components/FormAnalyticalSolutions/FormAnalyticalSolutions";
-import FormOurSolutions from "@/components/FormOurSolutions/FormOurSolutions";
 import { AppLayout } from "@/components/Layout";
-import { PlatformSolutions } from "@/components/PlatformSolutions/PlatformSolutions";
 import { SEO } from "@/components/Seo";
-import { SpeakWithSpecialist } from "@/components/SpeakWithSpecialist/SpeakWithSpecialist";
-import { SpecialistBanner } from "@/components/SpecialistBanner";
 
 const PrevencaoPerdas = () => {
   const idPage = "prevencao-perdas";
